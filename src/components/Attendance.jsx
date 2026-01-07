ecb2bf4d278a843ad3c24b93a44e8922f090fce9
0201Khushi <khushilupu05@gmail.com>
2026-01-07 07:01:35 +0000

    l

--- src/components/Attendance.jsx
+++ src/components/Attendance.jsx
@@ -1,8 +1,17 @@
 function Attendance() {
   return (
-    <div>
-      <h2>Attendance</h2>
-      <p>This is the attendance page.</p>
+     <div className="attendance-page">
+
+      {/* Header */}
+      <header className="top-bar">
+      <h2 style={{
+      fontFamily: "Jura, sans-serif",
+      fontSize: "22px",
+      fontWeight: "600",
+      color: "#000",
+      margin: 0,
+    }}>Attendance</h2>
+      </header>
     </div>
   );
 }
