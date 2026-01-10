1bfe23bb4860cdbc266910df68d6b3572669d2ff
0201Khushi <khushilupu05@gmail.com>
2026-01-10 20:44:22 +0000

    l

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -82,7 +82,7 @@ export default function Home() {
 
       {/* HEADER */}
       <div className="header">
-        <h1>{getGreeting()}, Khushi.</h1>
+        <h1>{getGreeting()},</h1>
         <p className="quote">“{quote}”</p>
       </div>
 
