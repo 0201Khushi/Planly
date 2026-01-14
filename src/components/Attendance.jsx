0c0bd6a72fc39642705010e9f6317f39ebbed6ff
0201Khushi <khushilupu05@gmail.com>
2026-01-14 21:52:04 +0000

    l

--- src/components/Attendance.jsx
+++ src/components/Attendance.jsx
@@ -462,8 +462,7 @@ const ProgressRing = ({ percentage }) => {
       <h3>Edit Attendance</h3>
 
       <div className="field">
-  <label className="text1" style={{
-            fontFamily: "Inter",}}>Classes Attended</label>
+  <label className="text1">Classes Attended</label>
   <input
     type="number"
     value={editAttended}
